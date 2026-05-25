50f8c91ad9366882fd1a134744c40f53a15ccb1c
suwapathiayur <suwapathiayur@gmail.com>
2026-05-25 09:46:51 +0530

    describe update

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+}
 
-const nextConfig: NextConfig = {};
-
-export default nextConfig;
+export default nextConfig
